7f97944ce263a9954f7e461bdab884fbdd7cc40f
hsmtkk <hsmtkk@gmail.com>
2023-10-24 16:42:10 +0900

    use external lib

--- main_test.go
+++ main_test.go
@@ -2,8 +2,14 @@ package main_test
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAlwaysOK(t *testing.T) {
 	// blank
+}
+
+func TestWithAssert(t *testing.T) {
+	assert.True(t, true)
 }
