6d85fb4bcf8871c1855a3ba94db412eb8fa77f3e
hsmtkk <hsmtkk@gmail.com>
2023-10-25 17:17:29 +0900

    add redis test code

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/redis/go-redis/v9"
@@ -22,4 +23,11 @@ func TestRedis(t *testing.T) {
 		DB:       0,
 	})
 	defer clt.Close()
+	ctx := context.Background()
+	err := clt.Set(ctx, "key", "value", 0).Err()
+	assert.Nil(t, err)
+
+	val, err := clt.Get(ctx, "key").Result()
+	assert.Nil(t, err)
+	assert.Equal(t, val, "value")
 }
